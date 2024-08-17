a31b3d39db558b8e0f3ec0c6ee5fb7b812eb3778
Vo Minh Khoi <mkhoi1908@gmail.com>
2024-08-17 21:35:12 +0700

    global error handling

---
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        return Problem();
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,10 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application.Services;
 using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +12,9 @@ builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
 
+//builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 builder.Services.AddControllers();
+builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +27,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
 
